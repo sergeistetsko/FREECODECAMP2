87aa5139e0d1e5d9a06a0e9e25c2f8e61df2daad
sergeistetsko <sergeistetsko2021@yandex.by>
2023-05-28 10:11:27 +0300

    Less 54

---
+++ BasicJavaScript/54.js
@@ -0,0 +1,12 @@
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const myOutfit = "sweater";
+  return myOutfit;
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
