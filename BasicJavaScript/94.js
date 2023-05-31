4ceab8c4b0c398e3aaedb419c61d1fd1ab7cc158
sergeistetsko <sergeistetsko2021@yandex.by>
2023-05-31 07:52:16 +0300

    Less 94

---
+++ BasicJavaScript/94.js
@@ -0,0 +1,15 @@
+// Setup
+const myPlants = [
+    {
+      type: "flowers",
+      list: ["rose", "tulip", "dandelion"]
+    },
+    {
+      type: "trees",
+      list: ["fir", "pine", "birch"]
+    }
+  ];
+  
+  // Only change code below this line
+  
+  const secondTree = myPlants[1].list[1];
