287785876b0e50065602c72c45a2ed328ff03c77
sergeistetsko <sergeistetsko2021@yandex.by>
2023-05-31 07:50:48 +0300

    Less 93

---
+++ BasicJavaScript/93.js
@@ -0,0 +1,14 @@
+const myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+    },
+    outside: {
+      trunk: "jack"
+    }
+  }
+};
+// Only change code below this line
+
+ const gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
