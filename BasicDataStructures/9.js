8a2b13f356587c75ae1fc2f50ef58fb4ca89c4d7
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-14 12:21:46 +0300

    9

---
+++ BasicDataStructures/9.js
@@ -0,0 +1,7 @@
+function spreadOut() {
+    let fragment = ["to", "code"];
+    let sentence = ["learning", ...fragment, "is", "fun"]; // change this line
+    return sentence;
+  }
+  
+  console.log(spreadOut());
