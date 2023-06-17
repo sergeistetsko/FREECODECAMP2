f73d2683d63f7e4e0054b2cb072516b9ed1ec08d
sergeistetsko <sergeistetsko2021@yandex.by>
2023-06-17 09:59:13 +0300

    13

---
+++ BasicAlgorithmScripting/13.js
@@ -0,0 +1,8 @@
+function bouncer(arr) {
+    const filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) filteredArr.push(arr[i]);
+    }
+    return filteredArr;
+  }
+  bouncer([7, "ate", "", false, 9]);
